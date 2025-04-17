fd5ec43987d760b0ef3c73037ff471ee2e6a249d
AdhritShetty <adhritshetty2009@gmail.com>
2025-04-17 20:39:53 +0000

    def

---
+++ calculator.py
@@ -0,0 +1,4 @@
+def calci(p,q):
+    print (p+q)
+calci(2,3)
+
---
+++ def.py
@@ -0,0 +1,14 @@
+def well_wishes (a,b):
+    return a+b
+print(well_wishes (2,4))
+
+def happy (z,x):
+    print(z-x)
+(happy (5,8))
+
+def weather ():
+    print("the weather is pleasant in",spring)
+    print("the weather is the same in ",autumn)
+spring="autumn"
+autumn="spring"
+weather ()
